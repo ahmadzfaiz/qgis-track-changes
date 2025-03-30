8e0265d9daa6ef0b7e0b946930c57412b683ba3f
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2025-03-30 14:36:36 +0700

    feat(main_ui): create toggle button to trigger dialog for gpkg (#32)

--- track_changes/code/main_plugin.py
+++ track_changes/code/main_plugin.py
@@ -2,7 +2,8 @@ import os
 from PyQt5.QtCore import Qt
 from qgis.PyQt.QtWidgets import QAction
 from qgis.PyQt.QtGui import QIcon
-from .default_logger_widget import FeatureLogger
+from .default_logger_widget import FeatureLogger as DefaultFeatureLogger
+from .gpkg_logger_widget import FeatureLogger as GpkgFeatureLogger
 from .about_widget import AboutWidget
 
 class TrackChangesPlugin:
@@ -25,7 +26,7 @@ class TrackChangesPlugin:
         self.default_log_action = QAction(QIcon(self.get_icon_path("../ui/icon/default.png")), "Default tracking", self.iface.mainWindow())
         self.default_log_action.triggered.connect(self.run_default)
         self.gpkg_log_action = QAction(QIcon(self.get_icon_path("../ui/icon/gpkg.png")), "GeoPackage tracking", self.iface.mainWindow())
-        # self.default_log_action.triggered.connect(self.run)
+        self.gpkg_log_action.triggered.connect(self.run_gpkg)
 
         # Add to QGIS menu
         self.iface.addPluginToMenu("Track Changes", self.about_action)
@@ -45,17 +46,28 @@ class TrackChangesPlugin:
         self.iface.removeToolBarIcon(self.default_log_action)
         self.iface.removeToolBarIcon(self.gpkg_log_action)
         self.default_log_action = None
+        self.gpkg_log_action = None
         self.about_action = None
 
     def run_default(self):
-        """Open the UI dialog."""
+        """Open the default logger UI dialog."""
         if self.default_log_dialog is None:
-            self.default_log_dialog = FeatureLogger()
+            self.default_log_dialog = DefaultFeatureLogger()
             self.iface.addDockWidget(Qt.RightDockWidgetArea, self.default_log_dialog)
             return
 
         # Toggle visibility
         self.default_log_dialog.setVisible(not self.default_log_dialog.isVisible())
+
+    def run_gpkg(self):
+        """Open the GeoPackage loggerUI dialog."""
+        if self.gpkg_log_dialog is None:
+            self.gpkg_log_dialog = GpkgFeatureLogger()
+            self.iface.addDockWidget(Qt.RightDockWidgetArea, self.gpkg_log_dialog)
+            return
+        
+        # Toggle visibility
+        self.gpkg_log_dialog.setVisible(not self.gpkg_log_dialog.isVisible())
 
     def about(self):
         """Show the About dialog."""
