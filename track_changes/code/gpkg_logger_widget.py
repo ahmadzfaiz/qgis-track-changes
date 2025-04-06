13316ea8e809d5906b35840e41193821550b67a2
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2025-04-06 19:10:20 +0700

    feat(gpkg_logger): add actions for attribute modifications (#32)

--- track_changes/code/gpkg_logger_widget.py
+++ track_changes/code/gpkg_logger_widget.py
@@ -65,6 +65,7 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
         self.layer_table_fields = {}
         for layer in QgsProject.instance().mapLayers().values():
             if self.gpkg_path in layer.source():
+                layer.setReadOnly(True)
                 table_name = layer.source().split("layername=")[-1].split("|")[0]
                 self.layers.append(layer)
                 self.layers_table.append(table_name)
@@ -74,8 +75,16 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
                     {"name": field.name(), "type": field.displayType()} 
                     for field in layer.fields()
                 ]
-        
         self.populate_list_layers()
+
+        # Get actual gpkg fields
+        self.commited_layer_table_fields = {}
+        for layer_name in self.layers_table:
+            gpkg_layer = QgsVectorLayer(f"{self.gpkg_path}|layername={layer_name}", layer_name, "ogr")
+            self.commited_layer_table_fields[layer_name] = [
+                {"name": field.name(), "type": field.displayType()}
+                for field in gpkg_layer.fields()
+            ]
     
     def on_file_selected(self, file_path):
         if file_path:
@@ -92,6 +101,10 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
         self.ui.pbDeactivate.setEnabled(True)
         self.ui.pbRefreshLayers.setEnabled(False)
         self.ui.mQgsLogFile.setEnabled(False)
+
+        # Enable editing all gpkg layers
+        for layer in self.layers:
+            layer.setReadOnly(False)
 
         # Start logging session
         self.gpkg_conn = sqlite3.connect(self.gpkg_path)
@@ -126,6 +139,10 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
         # Action 3*
         self.active_layer.attributeAdded.connect(self.log_attribute_added)
         self.active_layer.attributeDeleted.connect(self.log_attribute_deleted)
+        self.active_layer.attributeValueChanged.connect(self.log_attribute_value_changed)
+        self.active_layer.committedAttributesAdded.connect(self.log_committed_attributes_added)
+        self.active_layer.committedAttributesDeleted.connect(self.log_committed_attributes_deleted)
+        self.active_layer.committedAttributeValuesChanges.connect(self.log_committed_attribute_values_changes)
 
     def disconnect_actions(self, layer):
         # Actions 1*
@@ -140,6 +157,10 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
         # Action 3*
         layer.attributeAdded.disconnect(self.log_attribute_added)
         layer.attributeDeleted.disconnect(self.log_attribute_deleted)
+        layer.attributeValueChanged.disconnect(self.log_attribute_value_changed)
+        layer.committedAttributesAdded.disconnect(self.log_committed_attributes_added)
+        layer.committedAttributesDeleted.disconnect(self.log_committed_attributes_deleted)
+        layer.committedAttributeValuesChanges.disconnect(self.log_committed_attribute_values_changes)
 
     def deactivate(self):
         self.ui.pbActivate.setEnabled(True)
@@ -266,6 +287,38 @@ class FeatureLogger(QDockWidget, Ui_SetupTrackingChanges):
         field = self.layer_table_fields[self.active_layer_name][fid]
         self.logging_data(31, None, "remove field", json.dumps(field))
         self.layer_table_fields[self.active_layer_name].pop(fid)
+
+    def log_attribute_value_changed(self, fid, index, value):
+        field = self.layer_table_fields[self.active_layer_name][index]
+        att_object = {field["name"]: value}
+        self.logging_data(32, fid, "change attribute", json.dumps(att_object))
+
+    def log_committed_attributes_added(self, lid, attributes):
+        table_name = lid[:-37]
+        att_objects = [
+            {"name": field.name(), "type": field.displayType()}
+            for field in attributes
+        ]
+        self.commited_layer_table_fields[table_name].extend(att_objects)
+        self.logging_data(33, None, "commit add field", json.dumps(att_objects))
+
+    def log_committed_attributes_deleted(self, lid, attributes):
+        table_name = lid[:-37]
+        att_objects = []
+        for idx in sorted(attributes, reverse=True):
+            att_objects.append(self.commited_layer_table_fields[table_name][idx])
+            del self.commited_layer_table_fields[table_name][idx]
+        self.logging_data(34, None, "commit remove field", json.dumps(att_objects))
+
+    def log_committed_attribute_values_changes(self, lid, attributes):
+        table_name = lid[:-37]
+        att_objects = []
+        for fid, values in attributes.items():
+            data = {"fid": fid}
+            for cid, value in values.items():
+                data[self.layer_table_fields[table_name][cid]["name"]] = value
+            att_objects.append(data)
+        self.logging_data(35, None, "commit change attribute", json.dumps(att_objects))
 
     def logging_data(self, change_code, feature_id, message, data):
         self.gpkg_cursor.execute("""
